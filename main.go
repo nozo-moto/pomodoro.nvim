ed0f0d55f120290890a5dc8d9f99d1ba72a48fce
nozomi0966 <nozomotoitech@gmail.com>
2019-02-14 19:17:12 +0900

    fix

--- main.go
+++ main.go
@@ -13,13 +13,13 @@ func main() {
 		time.NewTicker(time.Second*command.NotifiactionTime),
 		startChan,
 	)
-	go pomodoro.Timer(int(command.PomodoroTime))
 	plugin.Main(func(p *plugin.Plugin) error {
 		p.HandleFunction(&plugin.FunctionOptions{Name: "PmdrStart"}, pomodoro.Start)
 		p.HandleFunction(&plugin.FunctionOptions{Name: "PmdrStop"}, pomodoro.Stop)
 		p.HandleFunction(&plugin.FunctionOptions{Name: "PmdrCancel"}, pomodoro.Cancel)
 		p.HandleFunction(&plugin.FunctionOptions{Name: "PmdrStatus"}, pomodoro.Status)
-
 		return nil
 	})
+
+	pomodoro.Timer(int(command.PomodoroTime))
 }
